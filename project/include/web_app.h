6c59db9486cfc087872f16592189753182f8e71f
knuts983 <knuts983@umn.edu>
2021-10-13 21:30:55 -0500

    Deleted all entity, interface, observer, strategy, and some utils from C++ codebase. Came from branch iteration210-5. Commented all environments except UMN in the html.

--- project/include/web_app.h
+++ project/include/web_app.h
@@ -1,58 +1,28 @@
 #ifndef WEB_APP_H_
 #define WEB_APP_H_
 
-#include "picojson.h"
 #include <map>
 #include "WebServer.h"
 #include <chrono>
-#include <vector>
-#include "interface/ientity.h"
-#include "util/vec3.h"
-#include "factory.h"
-#include <string>
-#include <stdexcept>
-#include <cassert>
-#include "util/console.h"
-#include "observer/observer.h"
 
 namespace csci3081 {
 
 // class Observer;
 
 class WebApp : public JSONSession {
-  public:
-    WebApp() : start(std::chrono::system_clock::now()), time(0.0), factory(new Factory()) {}
-    ~WebApp() { Console::Log(WARNING, "Webapp shutting down -- Ensure no memory leaks"); }
+public:
+    WebApp() : start(std::chrono::system_clock::now()), time(0.0) {}
+    ~WebApp() {}
+
     void receiveJSON(picojson::value& val);
     void ReceiveCommand(const std::string& cmd, picojson::object& data, picojson::object& returnValue);
     void Update(double dt, picojson::object& returnValue);
     void KeyUp(const std::string& key, int keyCode);
     void KeyDown(const std::string& key, int keyCode);
 
-    //iteration 2 functions
-    void AddEntity(Entity* e) { entities.push_back(e); Console::Log(SUCCESS, "entity successfully added!"); std::cout << entities.size() << std::endl;  }
-    const std::vector<Entity*> GetEntities() const { return entities; }
-
-    // functions for observer implementation
-    void NotifyObservers(picojson::value& notification, Entity* entity);
-    void RemoveObserver(Observer* observer);
-    void AddObserver(Observer* observer);
-    void SendNotification(const std::string& s) {
-      Console::Log(INFO, s);
-    }
-    void Test() {
-      printf("Ass!\n");
-    }
-
-
-  private:
+private:
     std::chrono::time_point<std::chrono::system_clock> start;
     double time;
-    std::vector<Entity*> entities;
-    Factory* factory;
-
-    // added this for efficiency as there will likely only ever be one actor
-    Actor* actor;
 };
 
 
--- project/src/web_app.cc
+++ project/src/web_app.cc
@@ -37,84 +37,18 @@ void WebApp::ReceiveCommand(const std::string& cmd, picojson::object& data, pico
     else if (cmd == "keydown") {
         KeyDown(data["key"].get<std::string>(), data["keyCode"].get<double>());
     }
-    else if (cmd == "image") {
-        const std::string& image = data.find("url")->second.get<std::string>();
-        // std::cout << image << std::endl;
-    }
-    else if (cmd == "reset") {
-        if (entities.size() > 0) {
-            entities.clear();
-        }
-    }
-    else if (cmd == "createEntity") {
-        assert(factory != nullptr);
-        // Entity* e = factory->Create(data.find("entity")->second.get<picojson::object>());
-        Entity* e = factory->Create(data);
-        Actor* actor = dynamic_cast<Actor*>(e);
-        if (actor != nullptr) {
-            Observer* o = new Observer(this);
-            actor->Attach(o);
-        }
-        if (e) { AddEntity(e); Console::Log(SUCCESS, "Entity was added!"); } else {
-            Console::Log(FAILURE, "Failed to add entity!");
-        }
-    } 
     else {
         std::cout << "Unknown command: " << cmd << " - " << picojson::value(data).serialize() << std::endl;
     }
 }
 
 void WebApp::Update(double dt, picojson::object& returnValue) {
-    // JsonHelper::Print(returnValue);
-    for (auto e : entities) {
-        Actor* actor = dynamic_cast<Actor*>(e);
-        if (actor != nullptr) {
-            actor->Update(dt);
-        } else {
-            e->Update(dt);
-        }
-        // std::cout << e->GetName() << std::endl;
-        // std::cout << e->Serialize() << std::endl;
-        returnValue["entity"+std::to_string(e->GetId())] = e->Serialize();
-    }
 }
 
 void WebApp::KeyUp(const std::string& key, int keyCode) {
-    std::cout << "key code up is: " << keyCode << std::endl;
-    if (actor) {
-        actor->Release(key, keyCode);
-    }
 }
 
 void WebApp::KeyDown(const std::string& key, int keyCode) {
-    std::cout << "key code down is: " << keyCode << std::endl;
-    if (actor) {
-        actor->Press(key, keyCode);
-    }
 }
-
-// void WebApp::AddObserver(Entity* e, Observer* observer) {
-//     Actor* actor = dynamic_cast<Actor*>(e);
-//     if (actor) {
-//         actor->AddObserver();
-//     }
-//   observers.push_back(observer);
-// }
-
-// void WebApp::RemoveObserver(Entity* e, Observer* observer) {
-
-//   for (int i = 0; i < observers.size(); i++) {
-//     if (observers[i] == observer) {
-//       observers.erase(observers.begin() + i);
-//       return;
-//     }
-//   }
-// }
-
-// void WebApp::NotifyObservers(picojson::value& notification, Entity* entity) {
-//   for (Observer* obs : observers) {
-//     obs->OnEvent(notification, *entity);
-//   }
-// }
 
 }
